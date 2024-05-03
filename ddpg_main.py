b3c51d5fba9caedc5bbeee7ec05d692c9ea4fbe3
Rahul Rustagi <rrustagi20@iitk.ac.in>
2024-05-03 11:17:47 +0530

    Add parser options

--- ddpg_main.py
+++ ddpg_main.py
@@ -5,39 +5,40 @@ import time
 import numpy as np
 from stable_baselines3 import DDPG
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
+from stable_baselines3.common.evaluation import evaluate_policy
+import argparse
 
 def main():
 
     env = gym.make('HardDriving-v0')
     action_noise=NormalActionNoise(mean=np.zeros(2), sigma=0.1 * np.ones(2))
-    model = DDPG("MlpPolicy", env, verbose=1, action_noise=action_noise ,tensorboard_log="./ddpg_test/")
+    model = DDPG("MlpPolicy", env, verbose=1, action_noise=action_noise ,tensorboard_log=f"./{args.logname}/")
     
     TIME_STEPS = 100_000
-    # for i in range(10):
-    i=1
-    model.learn(total_timesteps=TIME_STEPS,reset_num_timesteps=False, tb_log_name=f"ddpg_{i*TIME_STEPS}")
-    model.save(f"ddpg_{i*TIME_STEPS}")
-    # model.learn(total_timesteps=100_000)
+    model.learn(total_timesteps=TIME_STEPS,reset_num_timesteps=False, tb_log_name=f"ddpg_{TIME_STEPS}")
+    model.save(f"ddpg_{args.model}")
+    del model  # delete trained model to demonstrate loading
 
-    # vec_env = model.get_env()
-    # obs = vec_env.reset()
-    # for i in range(1000):
-    #     action, _states = model.predict(obs, deterministic=True)
-    #     obs, reward, done, info = vec_env.step(action)
-    #     vec_env.render()
-        # VecEnv resets automatically
-        # if done:
-        #   obs = vec_env.reset()
+    # Load the trained agent
+    model = DDPG.load(f"ddpg_{args.model}", env=env, print_system_info=True)
 
+    # Evaluate the agent
+    mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+    print(f"mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
 
-    # ob = env.reset()
-    # while True:
-    #     action = agent(ob)
-    #     ob, _, done, _ = env.step(action)
-    #     env.render()
-    #     if done:
-    #         ob = env.reset()
-    #         time.sleep(1/30)
+    # Enjoy trained agent
+    vec_env = model.get_env()
+    obs = vec_env.reset()
+    for i in range(1000):
+        action, _states = model.predict(obs, deterministic=True)
+        obs, rewards, dones, info = vec_env.step(action)
+        # print(f"Step: {i} Action: {action} Reward: {rewards} Done: {dones}")
+        vec_env.render("human")
 
 if __name__ == '__main__':
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--logname", default="ddpg_test")
+    parser.add_argument("--model", default="test1")                
+    args = parser.parse_args()
     main()
