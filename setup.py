80a2f8d5097872f1fe5b8806570404e942b23f2e
Rahul Rustagi <77167720+rrustagi20@users.noreply.github.com>
2024-05-03 10:46:19 +0530

    Minor Changes

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ setup(
     name="simple_driving",
     version="0.0.1",
     python_requires=">3.5.2",
-    install_requires=["gym=0.21.0",
-                      "pybullet=3.2.5",
-                      "numpy=1.19.5",
-                      "pandas=1.3.3",
-                      "matplotlib=3.3.4",
-                      "torch=1.10.2",
-                      "tensorboard=2.10.0",
-                      "stable-baselines3=1.3.0"
+    install_requires=["gym==0.21.0",
+                      "pybullet==3.2.5",
+                      "numpy==1.19.5",
+                      "pandas==1.1.5",
+                      "matplotlib==3.3.4",
+                      "torch==1.10.2",
+                      "tensorboard==2.10.0",
+                      "stable-baselines3==1.3.0"
                       ]
 )
